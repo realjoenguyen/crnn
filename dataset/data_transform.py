70d4f9181e971294d988f074b65fc30f988820ed
Huynh Vinh Loc <hvloc15@apcs.vn>
2019-03-20 22:20:29 +0700

    Add noise to image feature

--- dataset/data_transform.py
+++ dataset/data_transform.py
@@ -5,105 +5,128 @@ import cv2
 import torch
 import config
 
+
 class ToTensor(object):
-    def __call__(self, sample):
-        sample["img"] = torch.from_numpy(sample["img"].transpose((2, 0, 1))).float()
-        sample["seq"] = torch.Tensor(sample["seq"]).int()
-        return sample
+	def __call__(self, sample):
+		sample["img"] = torch.from_numpy(sample["img"].transpose((2, 0, 1))).float()
+		sample["seq"] = torch.Tensor(sample["seq"]).int()
+		return sample
 
 
 class Resize(object):
-    def __init__(self, size=(320, 32), data_augmen=False):
-        self.size = size
-        self.data_augmen = data_augmen
+	def __init__(self, size=(320, 32), data_augmen=False):
+		self.size = size
+		self.data_augmen = data_augmen
 
-    def __call__(self, sample):
-        img = sample["img"]
-        # cv2.imwrite(os.path.join(config.output_dir, sample["name"][:-4] + "_before.jpg"), img)
+	def __call__(self, sample):
+		img = sample["img"]
+		# cv2.imwrite(os.path.join(config.output_dir, sample["name"][:-4] + "_before.jpg"), img)
 
-        # increase dataset size by applying random stretches to the images
-        if self.data_augmen:
-            stretch = (random.random() - 0.5)  # -0.5 .. +0.5
-            wStretched = max(int(img.shape[1] * (1 + stretch)), 1)  # random width, but at least 1
-            img = cv2.resize(img, (wStretched, img.shape[0]))  # stretch horizontally by factor 0.5 .. 1.5
+		# increase dataset size by applying random stretches to the images
+		if self.data_augmen:
+			stretch = (random.random() - 0.5)  # -0.5 .. +0.5
+			wStretched = max(int(img.shape[1] * (1 + stretch)), 1)  # random width, but at least 1
+			img = cv2.resize(img, (wStretched, img.shape[0]))  # stretch horizontally by factor 0.5 .. 1.5
 
-        (wt, ht) = self.size
-        (h, w, c) = img.shape
-        fx = w / wt
-        fy = h / ht
-        f = max(fx, fy)
-        newSize = (max(min(wt, int(w / f)), 1),
-                   max(min(ht, int(h / f)), 1))  # scale according to f (result at least 1 and at most wt or ht)
-        img = cv2.resize(img, newSize)
-        target = np.ones([ht, wt, 3]) * 255
+		(wt, ht) = self.size
+		(h, w, c) = img.shape
+		fx = w / wt
+		fy = h / ht
+		f = max(fx, fy)
+		newSize = (max(min(wt, int(w / f)), 1),
+				   max(min(ht, int(h / f)), 1))  # scale according to f (result at least 1 and at most wt or ht)
+		img = cv2.resize(img, newSize)
+		target = np.ones([ht, wt, 3]) * 255
 
-        if self.data_augmen:
-            left_upper_h = random.randint(0, ht - newSize[1])
-            left_upper_w = random.randint(0, wt - newSize[0])
-            target[left_upper_h:newSize[1] + left_upper_h, left_upper_w:newSize[0] + left_upper_w, :] = img
-        else:
-            target[0:newSize[1], 0:newSize[0], :] = img
+		if self.data_augmen:
+			left_upper_h = random.randint(0, ht - newSize[1])
+			left_upper_w = random.randint(0, wt - newSize[0])
+			target[left_upper_h:newSize[1] + left_upper_h, left_upper_w:newSize[0] + left_upper_w, :] = img
+		else:
+			target[0:newSize[1], 0:newSize[0], :] = img
 
-        # # normalize
-        # (m, s) = cv2.meanStdDev(img)
-        # m = int(m[0][0])
-        # s = s[0][0]
-        # img = img - m
-        # # img = int(img / s) if s > 0 else img
-        # img = np.rint(img / s) if s > 0 else img
-        # sample["img"] = cv2.resize(sample["img"], self.size)
-        # visualize
-        # print ("print sample to", os.path.join(config.output_dir, sample["name"]))
+		# # normalize
+		# (m, s) = cv2.meanStdDev(img)
+		# m = int(m[0][0])
+		# s = s[0][0]
+		# img = img - m
+		# # img = int(img / s) if s > 0 else img
+		# img = np.rint(img / s) if s > 0 else img
+		# sample["img"] = cv2.resize(sample["img"], self.size)
+		# visualize
+		# print ("print sample to", os.path.join(config.output_dir, sample["name"]))
 
-        # cv2.imwrite(os.path.join(config.output_dir, sample["name"][:-4] + "_after.jpg"), target)
-        sample["img"] = target
-        return sample
+		# cv2.imwrite(os.path.join(config.output_dir, sample["name"][:-4] + "_after.jpg"), target)
+		sample["img"] = target
+		return sample
 
 
 class Rotation(object):
-    def __init__(self, angle=20, fill_value=0, p = 0.5):
-        self.angle = angle
-        self.fill_value = fill_value
-        self.p = p
+	def __init__(self, angle=20, fill_value=0, p=0.5):
+		self.angle = angle
+		self.fill_value = fill_value
+		self.p = p
 
-    def __call__(self, sample):
-        if np.random.uniform(0.0, 1.0) < self.p or not sample["aug"]:
-            return sample
-        h,w,_ = sample["img"].shape
-        ang_rot = np.random.uniform(self.angle) - self.angle/2
-        transform = cv2.getRotationMatrix2D((w/2, h/2), ang_rot, 1)
-        sample["img"] = cv2.warpAffine(sample["img"], transform, (w,h), borderValue = self.fill_value)
-        return sample
+	def __call__(self, sample):
+		if np.random.uniform(0.0, 1.0) < self.p or not sample["aug"]:
+			return sample
+		h, w, _ = sample["img"].shape
+		ang_rot = np.random.uniform(self.angle) - self.angle / 2
+		transform = cv2.getRotationMatrix2D((w / 2, h / 2), ang_rot, 1)
+		sample["img"] = cv2.warpAffine(sample["img"], transform, (w, h), borderValue=self.fill_value)
+		return sample
 
 
 class Translation(object):
-    def __init__(self, fill_value=0, p = 0.5):
-        self.fill_value = fill_value
-        self.p = p
+	def __init__(self, fill_value=0, p=0.5):
+		self.fill_value = fill_value
+		self.p = p
 
-    def __call__(self, sample):
-        if np.random.uniform(0.0, 1.0) < self.p or not sample["aug"]:
-            return sample
-        h,w,_ = sample["img"].shape
-        trans_range = [w / 10, h / 10]
-        tr_x = trans_range[0]*np.random.uniform()-trans_range[0]/2
-        tr_y = trans_range[1]*np.random.uniform()-trans_range[1]/2
-        transform = np.float32([[1,0, tr_x], [0,1, tr_y]])
-        sample["img"] = cv2.warpAffine(sample["img"], transform, (w,h), borderValue = self.fill_value)
-        return sample
+	def __call__(self, sample):
+		if np.random.uniform(0.0, 1.0) < self.p or not sample["aug"]:
+			return sample
+		h, w, _ = sample["img"].shape
+		trans_range = [w / 10, h / 10]
+		tr_x = trans_range[0] * np.random.uniform() - trans_range[0] / 2
+		tr_y = trans_range[1] * np.random.uniform() - trans_range[1] / 2
+		transform = np.float32([[1, 0, tr_x], [0, 1, tr_y]])
+		sample["img"] = cv2.warpAffine(sample["img"], transform, (w, h), borderValue=self.fill_value)
+		return sample
 
 
 class Scale(object):
-    def __init__(self, scale=[0.5, 1.2], fill_value=0, p = 0.5):
-        self.scale = scale
-        self.fill_value = fill_value
-        self.p = p
+	def __init__(self, scale=[0.5, 1.2], fill_value=0, p=0.5):
+		self.scale = scale
+		self.fill_value = fill_value
+		self.p = p
 
-    def __call__(self, sample):
-        if np.random.uniform(0.0, 1.0) < self.p or not sample["aug"]:
-            return sample
-        h, w, _ = sample["img"].shape
-        scale = np.random.uniform(self.scale[0], self.scale[1])
-        transform = np.float32([[scale, 0, 0],[0, scale, 0]])
-        sample["img"] = cv2.warpAffine(sample["img"], transform, (w,h), borderValue = self.fill_value)
-        return sample
+	def __call__(self, sample):
+		if np.random.uniform(0.0, 1.0) < self.p or not sample["aug"]:
+			return sample
+		h, w, _ = sample["img"].shape
+		scale = np.random.uniform(self.scale[0], self.scale[1])
+		transform = np.float32([[scale, 0, 0], [0, scale, 0]])
+		sample["img"] = cv2.warpAffine(sample["img"], transform, (w, h), borderValue=self.fill_value)
+		return sample
+
+
+class AddNoise:
+
+	def __init__(self, probability=0.2):
+		self.probability = probability
+
+	def __call__(self, sample):
+		image = sample['img']
+		image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+		h, w = image.shape
+		for i in range(h):
+			for j in range(w):
+				if image[i][j] < 100:
+					rdn = random.random()
+					if rdn < self.probability:
+						h1 = random.randint(2, 5)
+						for i_1 in range(i, min(i + h1, h)):
+							w1 = random.randint(1, 5)
+							for j_1 in range(j, min(j + w1, w)):
+								image[i_1][j_1] = 255
+		return image
